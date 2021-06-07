5188a6f468f31419e7fa3c48a2dd3768982dd1ed
Sonu Yadav <sonukyadav2510@gmail.com>
2021-06-07 16:12:07 +0530

    Update dbconn.php

--- About/profile/edit/about/databases/dbconn.php
+++ About/profile/edit/about/databases/dbconn.php
@@ -1,11 +1,15 @@
 <?php 
-$conn = mysqli_connect('localhost','root','','linkdmeknit');  
+ $host ='remotemysql.com';
+ $user ='sQCh2RxZz4';
+ $pwd='Y0ZIrGpfkq';
+ $db ='sQCh2RxZz4';
+ $conn = mysqli_connect($host,$user,$pwd,$db);
 
-if($conn == true){
-  
-}
-else{
-    echo("connection failed !!");
-}
+ if($conn == true){
+
+ }
+ else{
+     echo("connection failed !!");
+ }
 
 ?>  
