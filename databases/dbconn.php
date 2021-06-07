ca229b7331e04cc20e3c1f5e5ac1dbe294a13cff
Sonu Yadav <sonukyadav2510@gmail.com>
2021-06-07 16:26:32 +0530

    Update dbconn.php

--- databases/dbconn.php
+++ databases/dbconn.php
@@ -2,7 +2,7 @@
  $host ='remotemysql.com';
  $user ='sQCh2RxZz4';
  $pwd='Y0ZIrGpfkq';
- $db ='sQCh2RxZz4';
+ $db ='sQCh2RxZz';
  $conn = mysqli_connect($host,$user,$pwd,$db);
 
  if($conn == true){
